2a6e4cb841817766bf80eda207843953cb61b985
Jason Tiernan <jason.tiernan@me.com>
2013-10-16 02:23:41 -0400

    Updated everything
    Think I broke the bird bounce thing though between leaving hte office
    and recommitting

---
+++ app.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    $('.slide:gt(0)').hide();
+    setInterval(function() {
+        $(".slide:first-child").fadeOut(5000).next(".slide").fadeIn(5000).end().appendTo(".slideshow")
+}, 5000);
+
+
+
+$("input").focus(
+	function() {
+		$(".bird").addClass('bounceAdd');
+		setTimeout(function() {
+			$(".bird").removeClass('bounceAdd');
+		}, 1000 );
+	}
+
+);
+
+       
+});
